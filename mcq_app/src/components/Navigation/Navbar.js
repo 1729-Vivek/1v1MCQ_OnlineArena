9bc49d50902b96363fda7efa423310c58707c47a
Vivek <itspersonal4vivek@gmail.com>
2024-06-26 13:28:58 +0530

    after clicking on logout it will redirect to signup page

--- mcq_app/src/components/Navigation/Navbar.js
+++ mcq_app/src/components/Navigation/Navbar.js
@@ -1,9 +1,15 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 
 const Navbar = () => {
   const { authToken, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
 
   return (
     <nav>
@@ -11,7 +17,7 @@ const Navbar = () => {
       {authToken ? (
         <>
           <Link to="/mcqs">MCQ Management</Link>
-          <button onClick={logout}>Logout</button>
+          <button onClick={handleLogout}>Logout</button>
         </>
       ) : (
         <>
